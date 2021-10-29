0031445758c734296aa5392fada6d1e3d0f4a411
ManoharKontham <kontham.manohar@gmail.com>
2021-10-29 17:05:31 +0530

    updated

--- Blurry Loading/script.js
+++ Blurry Loading/script.js
@@ -1,20 +1,20 @@
 const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
 
-let load = 0
+let load = 100
 
 let int = setInterval(blurring, 30)
 
 function blurring() {
-  load++
+  load--
 
-  if (load > 99) {
+  if (load < 1) {
     clearInterval(int)
   }
 
   loadText.innerText = `${load}%`
-  loadText.style.opacity = scale(load, 0, 100, 1, 0)
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+  loadText.style.opacity = scale(load, 0, 100, 0, 1)
+  bg.style.filter = `blur(${scale(load, 0, 100, 1, 30)}px)`
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
