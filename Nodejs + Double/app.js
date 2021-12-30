56bc11c022fa5704b822ccd3e0603b397ff3d15d
ManoharKontham <92983324+ManoharKontham@users.noreply.github.com>
2021-12-30 23:01:16 +0530

    uploaded

--- Nodejs + Double/app.js
+++ Nodejs + Double/app.js
@@ -1,22 +1,41 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const path = require('path');
-const cors = require('cors');
-const bodParser = require('body-parser');
-const indexRoutes = require('./routes/index');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+const dbcon = require('./core/db')
 
-const app = express();
+var indexRouter = require('./routes/index');
+var app = express();
 
-app.use(expressLayouts);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: true}));
-app.use(cors());
-app.use(bodParser.json());
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(indexRoutes.routes);
-//app.use(authRoutes.routes);
+
+app.use('/', indexRouter);
 
 
-app.listen(3000, () => console.log('App is listening on url http://localhost:3000'));
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
--- Nodejs + Double/routes/index.js
+++ Nodejs + Double/routes/index.js
@@ -1,18 +1,38 @@
 var express = require('express');
+const doubles = require('../models/double');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+ router.get('/', function(req, res, next) {
+   res.render('index', { title: 'Express' });
+ });
 
-router.get('/', function(req, res, next) {
+router.get('/1', function(req, res, next) {
   //es.send('respond with a resource');
-  res.render('index', { title: 'Express'});
-  double.find({}, function(err, rows){
-    console.log(rows)
-  });
-  
+  doubles.find({}, function(err, rows){
+    console.log(rows[0].url)
+    const picUrl= rows[0].url;
+    const picUrl1= rows[1].url;
+    const picUrl2= rows[2].url;
+    const picUrl3= rows[3].url;
+
+    const head1 = rows[0].h;
+    const head2 = rows[1].h;
+    const head3 = rows[2].h;
+    const head4 = rows[3].h;
+    
+    const para1 = rows[0].p;
+    const para2 = rows[1].p;
+    const para3 = rows[2].p;
+    const para4 = rows[3].p;
+
+
+    res.render('index',{picUrl,picUrl1,picUrl2,picUrl3,head1,head2,head3,head4,para1,para2,para3,para4})
+});
 });
 
+
 module.exports = router;
+
+
+
